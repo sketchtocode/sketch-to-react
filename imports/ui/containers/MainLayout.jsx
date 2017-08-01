684844acb3f24cc0315fa9a7651142f2faded6f1
Arkadiusz Janeczko <ajaneczko1@gmail.com>
2017-08-01 08:58:56 +0200

    Import section in Main Layout

--- imports/ui/containers/MainLayout.jsx
+++ imports/ui/containers/MainLayout.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Header from '../components/Header.jsx';
-import Home from '../pages/Home.jsx';
-import About from '../pages/About.jsx';
-import NotFound from '../pages/NotFound.jsx';
+import SectionHeroUnit from '../components/section--hero-unit/HeroUnit.jsx';
+
 
 export default class MainLayout extends React.Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Header />
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path = '/about' component={About} />
-              <Route component={NotFound} />
-            </Switch>
-        </div>
-      </Router>
+      <div>
+          <SectionHeroUnit/>
+      </div>
     );
   }
 }
